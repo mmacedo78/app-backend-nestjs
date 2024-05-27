7a70a846f9b0c6d1e13ca6cda7d1e665e90a7187
Marcelo Macedo <mmacedo78@gmail.com>
2024-05-27 16:04:38 -0300

    ...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
@@ -13,7 +11,7 @@ import { DatabaseModule } from './database/database.module';
     DatabaseModule,
     UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
